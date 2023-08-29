ab5dc3e2f4bf4eabf29bdddd6ca5bf34f0dafaa4
George Bockari <bitwhys@gmail.com>
2023-08-29 15:34:30 -0400

    refactor(MarketingLayout): remove hideFooter prop

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,11 +2,10 @@ import Footer from "@/components/footer"
 import Header from "@/components/header"
 
 type LayoutProps = {
-  hideFooter?: boolean
   children: React.ReactNode
 }
 
-const MarketingLayout = ({ hideFooter, children }: LayoutProps) => (
+const MarketingLayout = ({ children }: LayoutProps) => (
   <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
     <Header />
     {children}
